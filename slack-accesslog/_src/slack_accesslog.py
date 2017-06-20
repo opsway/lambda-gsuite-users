1bdc040b6e5a3cb0c2885aad89c29b959056a5a0
Andriy Samilyak <samilyak@gmail.com>
2017-06-20 08:47:17 +0200

    More refactoring

--- slack-accesslog/_src/slack_accesslog.py
+++ slack-accesslog/_src/slack_accesslog.py
@@ -1,48 +1,47 @@
 import requests
-import json
 import time
 import os
+import sys
+import tempfile
+import base64
+import csv
 import datetime
+import json
+
+
+# If python gives error "Can not find module google", just add file __init__.py to google and google/cloud directory
 from google.cloud import bigquery
 
-def get_logins_with_pagination(url):
-    values = []
-    page = 1
 
-    while True:         
-        r = requests.get(url + '?count=1000&token=' + os.environ['SLACK_TOKEN'] + '&page=' + str(page))
-        result = r.json();
-
-        if (r.status_code != 200):
-            raise ValueError('Error retrieving date')    
-
-        if ('logins' in result):
-            values = values + result['logins']
-        else:
-            break;
-
-        if (result['paging']['page'] < result['paging']['pages']):
-            page = page + 1;
-        else:
-            break;
-    return values;
-
-
-def main(event, context):
-    f = open('client_secret.json', 'w')
+def auth_google_cloud_sdk():
+    # Copy file with google secret from Lambda environment settings
+    google_secret_file = tempfile.mkdtemp() + '/client_secret.json'
+    f = open(google_secret_file, 'w')
     f.write(base64.b64decode(os.environ.get('GOOGLE_CLIENT_SECRET_CONTENTS')))
     f.close()
+    
+    os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = google_secret_file
 
-    table = bigquery.Client().dataset('slack').table('presence_partitioned')
+
+
+def export_slackaccesslog(event, context):
+    auth_google_cloud_sdk()
+
+    table = bigquery.Client().dataset('slack').table('logins_partitioned')
     table.reload()
     rows = []
     for login in get_logins_with_pagination('https://slack.com/api/team.accessLogs'):
-        if (time.time() - float(login['date_first']) < 48 * 60 * 60):
+        if (time.time() - float(login['date_first']) < 48 * 60):
         # We need 48 hours of recent history, as Slack does not allow sessions longer than 24 hours
-            login['date_first'] = time.strftime('%Y-%m-%d %H:%M:%S UTC', time.gmtime(login['date_first']))
-            login['date_last'] = time.strftime('%Y-%m-%d %H:%M:%S UTC', time.gmtime(login['date_last'])) 
-            rows.append(login)
+            login['date_first'] = time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime(login['date_first']))
+            login['date_last'] = time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime(login['date_last'])) 
+            
+            rows.append((login['username'],login['count'],login['user_id'],
+                login['ip'],login['region'], login['isp'], login['user_agent'], login['country'], 
+                login['date_first'], login['date_last']))
 
-        errors = table.insert_data(rows)
-        if errors:
-            raise Exception(errors)
+    print "Sending " + str(len(rows)) + " records to BigQuery ..."
+    errors = table.insert_data(rows)
+    if errors:
+        print (errors)
+        raise Exception(errors)
